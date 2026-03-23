1f6fcdf816ac2cd17c016b976c07ae4fc9c89746
Soumyajit Bera <129548524+Soumyajit003@users.noreply.github.com>
2026-03-23 23:42:12 +0530

    Add comment for search form clarity
    Added a comment to clarify the purpose of the form.

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -16,6 +16,7 @@ const SearchBar = ({ data }) => {
   }
 
   return (
+    // it is for searching
     <form onSubmit={onSearchHandler} className="max-w-xl w-full md:h-14 h-12 flex items-center bg-transparent/5 backdrop-blur-lg border border-white/10 shadow-lg shadow-purple-900/20 rounded-xl">
       <img className="md:w-auto w-10 px-3" src={assets.search_icon} alt="search_icon" />
       <input onChange={e => setInput(e.target.value)} value={input} type="text" className="w-full h-full outline-none text-gray-200 bg-transparent" placeholder="Search for courses" />
