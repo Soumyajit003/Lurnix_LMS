5672091718072058bde864b86effbeaf2f37bc4d
Soumyajit003 <soumya.bera003@gmail.com>
2026-04-11 19:40:40 +0530

    update

--- server/models/Purchase.js
+++ server/models/Purchase.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const PurchaseSchema = new mongoose.Schema({
-    courseId: { type: mongoose.Schema.Types.ObjectId,
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Course',
         required: true
     },
